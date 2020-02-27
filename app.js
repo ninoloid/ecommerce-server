c4379398a648c7ada90484e5ac23f5dd8ccc35bb
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-02-27 12:03:45 +0700

    thankspage

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
 
+// CRON JOB
+const CronJob = require('cron').CronJob;
+const moment = require('moment');
+const { Cart } = require('./models')
+const { Op } = require('sequelize')
+moment().format()
+
+const job = new CronJob('0 */15 * * * *', function () {
+  Cart.destroy({ where: { 
+    createdAt: {
+      [Op.lte] : moment().subtract(1, 'hours').toDate()
+    },
+    checkout: false
+  } })
+    .then(() => {
+      console.log('Cart deleted')
+    })
+  }, null, true, 'Asia/Jakarta');
+job.start();
+// END OF CRON JOB
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
